b938d532a1cf52be4fc9777f48444b685b8d8802
Kapibara-Bba <kapibara.bba0924@gmail.com>
2024-07-31 00:26:16 +0900

    タップ時のラベルテキスト変更

--- Sampleios/ViewController.swift
+++ Sampleios/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func dogButton(_ sender: Any) {
+        label.text = "ワンワン"
+    }
+    @IBAction func catButton(_ sender: Any) {
+        label.text = "にゃんにゃん"
+    }
+    @IBAction func frogButton(_ sender: Any) {
+        label.text = "ゲロゲロ"
+    }
+    
 }
 
