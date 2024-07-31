75808877a9008364ef5318cf4d49adfdb91982bb
Kapibara-Bba <kapibara.bba0924@gmail.com>
2024-08-01 00:02:30 +0900

    プッシュ遷移、モーダル遷移

--- Sampleios/ViewController.swift
+++ Sampleios/ViewController.swift
@@ -9,21 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func dogButton(_ sender: Any) {
-        label.text = "ワンワン"
-    }
-    @IBAction func catButton(_ sender: Any) {
-        label.text = "にゃんにゃん"
-    }
-    @IBAction func frogButton(_ sender: Any) {
-        label.text = "ゲロゲロ"
-    }
-    
+
 }
 
